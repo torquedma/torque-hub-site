e4128fa15e8257f1012f3c766fd526ae59ae6638
Ryan Davis <ryan.davis@torquedma.com>
2026-05-25 23:31:55 -0400

    category.js: add Sleeper Tractor to semi-trucks-for-sale subs (recovers 4 orphaned sleeper units)

--- netlify/edge-functions/category.js
+++ netlify/edge-functions/category.js
@@ -242,7 +242,7 @@ const CATEGORY_HUBS = {
 
 const SUBCATEGORY_LEAVES = {
   'box-trucks-for-sale':             { subs: ['Box Truck'],                     label: 'Box Trucks',           hub: 'trucks-for-sale' },
-  'semi-trucks-for-sale':            { subs: ['Day Cab Tractor'],               label: 'Semi Trucks',          hub: 'trucks-for-sale' },
+  'semi-trucks-for-sale':            { subs: ['Day Cab Tractor','Sleeper Tractor'], label: 'Semi Trucks',          hub: 'trucks-for-sale' },
   'service-trucks-for-sale':         { subs: ['Service Truck'],                 label: 'Service Trucks',       hub: 'trucks-for-sale' },
   'dump-trucks-for-sale':            { subs: ['Dump Truck','Grain Dump Truck'], label: 'Dump Trucks',          hub: 'trucks-for-sale' },
   'cab-and-chassis-trucks-for-sale': { subs: ['Cab & Chassis'],                 label: 'Cab & Chassis Trucks', hub: 'trucks-for-sale' },
